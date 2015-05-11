93abf2f4bf2dde9af6ebc14ca2f3507134ac1edf
JesseHsiu <jesse801122@gmail.com>
2015-05-11 20:08:45 +0800

    Initial Commit

---
+++ Forceboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Forceboard
+//
+//  Created by 修敏傑 on 2015/5/11.
+//  Copyright (c) 2015年 jessehsiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Forceboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Forceboard
+//
+//  Created by 修敏傑 on 2015/5/11.
+//  Copyright (c) 2015年 jessehsiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
