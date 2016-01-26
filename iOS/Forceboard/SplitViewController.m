8dea5f498f2f91c96156dca51bdf628ac7a239a2
JesseHsiu <jesse801122@gmail.com>
2016-01-26 17:39:02 +0800

    split add swipe path and swipe direction

--- iOS/Forceboard/SplitViewController.m
+++ iOS/Forceboard/SplitViewController.m
@@ -10,7 +10,6 @@
 
 @implementation SplitViewController
 
-
 -(void)viewDidLoad
 {
     [super viewDidLoad];
@@ -57,11 +56,36 @@
     if (swipeGestureRecognizer.direction == UISwipeGestureRecognizerDirectionRight ) {
         isCurrentLeft = YES;
         middleLineConstraint.constant = -228;
+        [keySequence addObject:@"->"];
+        
+        NSMutableArray *array_x = [[NSMutableArray alloc] init];
+        NSMutableArray *array_y = [[NSMutableArray alloc] init];
+        
+        for (int i = 0; i< [pathRecordtmp count]; i++) {
+            [array_x addObject:[pathRecordtmp objectAtIndex:i][0]];
+            [array_y addObject:[pathRecordtmp objectAtIndex:i][1]];
+        }
+        NSDictionary *dict = [[NSDictionary alloc]initWithObjectsAndKeys:array_x, @"xs", array_y, @"ys", @"right", @"type", nil];
+        [pathRecordBeforeNext addObject:dict];
+        [pathRecordtmp removeAllObjects];
+        
     }
     else if (swipeGestureRecognizer.direction == UISwipeGestureRecognizerDirectionLeft)
     {
+        [keySequence addObject:@"<-"];
         isCurrentLeft = NO;
         middleLineConstraint.constant = -136;
+        
+        NSMutableArray *array_x = [[NSMutableArray alloc] init];
+        NSMutableArray *array_y = [[NSMutableArray alloc] init];
+        
+        for (int i = 0; i< [pathRecordtmp count]; i++) {
+            [array_x addObject:[pathRecordtmp objectAtIndex:i][0]];
+            [array_y addObject:[pathRecordtmp objectAtIndex:i][1]];
+        }
+        NSDictionary *dict = [[NSDictionary alloc]initWithObjectsAndKeys:array_x, @"xs", array_y, @"ys", @"left", @"type", nil];
+        [pathRecordBeforeNext addObject:dict];
+        [pathRecordtmp removeAllObjects];
     }
     
     [UIView animateWithDuration:0.2 animations:^{
